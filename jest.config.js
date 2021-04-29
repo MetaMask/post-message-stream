2d2a0563dc2d7b6cdf2320213e34f45965270f99
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2021-04-28 20:55:22 -0700

    Standardize repository (#16)
    * Standardize repository
    
    * Clean up gitignore
    
    * fixup! Clean up gitignore

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // TODO: Fix test coverage
+  // collectCoverage: true,
+  // coverageReporters: ['text', 'html'],
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 100,
+  //     functions: 100,
+  //     lines: 100,
+  //     statements: 100,
+  //   },
+  // },
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  preset: 'ts-jest',
+  // "resetMocks" resets all mocks, including mocked modules, to jest.fn(),
+  // between each test case.
+  resetMocks: true,
+  // "restoreMocks" restores all mocks created using jest.spyOn to their
+  // original implementations, between each test. It does not affect mocked
+  // modules.
+  restoreMocks: true,
+  testEnvironment: 'node',
+  testRegex: ['\\.test\\.(ts|js)$'],
+  testTimeout: 2500,
+};
