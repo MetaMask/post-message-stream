001782d63ac019fc58eed2d5c3fd981ec5193dfc
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2021-06-25 17:50:58 -0700

    Update src/WindowPostMessageStream.ts

--- src/WindowPostMessageStream.ts
+++ src/WindowPostMessageStream.ts
@@ -7,8 +7,8 @@ import {
 interface WindowPostMessageStreamArgs {
   name: string;
   target: string;
-  targetWindow: Window;
-  targetOrigin: string;
+  targetWindow?: Window;
+  targetOrigin?: string;
 }
 
 /**
