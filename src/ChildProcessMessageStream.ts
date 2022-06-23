4a48264fa67ac957a8c3738c7dec3ceaa1d23559
Frederik Bolding <frederik.bolding@gmail.com>
2022-06-23 16:23:06 +0200

    Add child_process streams

---
+++ src/ChildProcessMessageStream.ts
@@ -0,0 +1,30 @@
+import { BasePostMessageStream, StreamData } from "./BasePostMessageStream";
+
+export class ChildProcessMessageStream extends BasePostMessageStream {
+  constructor() {
+    super();
+
+    this._onMessage = this._onMessage.bind(this);
+    globalThis.process.on('message', this._onMessage);
+
+    this._handshake();
+  }
+
+  protected _postMessage(data: StreamData): void {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    globalThis.process.send!({ data });
+  }
+
+  private _onMessage(message: any): void {
+    // validate message
+    if (typeof message !== 'object' || !message.data) {
+      return;
+    }
+
+    this._onData(message.data as StreamData);
+  }
+
+  _destroy(): void {
+    globalThis.process.removeListener('message', this._onMessage);
+  }
+}
---
+++ src/ChildProcessParentMessageStream.ts
@@ -0,0 +1,37 @@
+import { ChildProcess } from 'child_process';
+import { BasePostMessageStream, StreamData } from './BasePostMessageStream';
+
+type ChildProcessParentMessageStreamArgs = {
+  process: ChildProcess;
+};
+
+export class ChildProcessParentMessageStream extends BasePostMessageStream {
+  private _process: ChildProcess;
+
+  constructor({ process }: ChildProcessParentMessageStreamArgs) {
+    super();
+
+    this._process = process;
+    this._onMessage = this._onMessage.bind(this);
+    this._process.on('message', this._onMessage);
+
+    this._handshake();
+  }
+
+  protected _postMessage(data: StreamData): void {
+    this._process.send({ data });
+  }
+
+  private _onMessage(message: any): void {
+    // validate message
+    if (typeof message !== 'object' || !message.data) {
+      return;
+    }
+
+    this._onData(message.data as StreamData);
+  }
+
+  _destroy(): void {
+    this._process.removeListener('message', this._onMessage);
+  }
+}
