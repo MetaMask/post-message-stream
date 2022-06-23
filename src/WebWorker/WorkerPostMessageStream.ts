15db8ce4f2574a913d3feb93ac602a2712a9e8e8
Erik Marks <rekmarks@protonmail.com>
2022-06-23 16:22:35 -0700

    Remove istanbul ignore directives

--- src/WebWorker/WorkerPostMessageStream.ts
+++ src/WebWorker/WorkerPostMessageStream.ts
@@ -1,4 +1,3 @@
-/* istanbul ignore file */
 // We ignore coverage for the entire file due to limits on our instrumentation,
 // but it is in fact covered by our tests.
 import {
@@ -45,7 +44,7 @@ export class WorkerPostMessageStream extends BasePostMessageStream {
       return;
     }
 
-    this._onData(message.data as StreamData);
+    this._onData(message.data);
   }
 
   // worker stream lifecycle assumed to be coterminous with global scope
