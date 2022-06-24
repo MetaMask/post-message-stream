b9769781e0da1d9af149dd7df1908a5e8650fbc5
Erik Marks <rekmarks@protonmail.com>
2022-06-23 17:01:43 -0700

    import type

--- src/node-process/ParentProcessMessageStream.ts
+++ src/node-process/ParentProcessMessageStream.ts
@@ -1,4 +1,4 @@
-import { ChildProcess } from 'child_process';
+import type { ChildProcess } from 'child_process';
 import { BasePostMessageStream } from '../BasePostMessageStream';
 import { isValidStreamMessage, StreamData } from '../utils';
 
