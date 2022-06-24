6051a208b65a9eaa0c5f49fd202d8fc5425754e5
Frederik Bolding <frederik.bolding@gmail.com>
2022-06-24 13:04:09 +0200

    Add export

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ export * from './WebWorker/WorkerPostMessageStream';
 export * from './WebWorker/WorkerParentPostMessageStream';
 export * from './node-process/ParentProcessMessageStream';
 export * from './node-process/ChildProcessMessageStream';
+export * from './node-thread/ParentThreadMessageStream';
+export * from './node-thread/ThreadMessageStream';
 export { PostMessageEvent } from './BasePostMessageStream';
 export { StreamData, StreamMessage } from './utils';
