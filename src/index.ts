32769ea91e9117bf9acd10bac6f19763329f334f
Frederik Bolding <frederik.bolding@gmail.com>
2022-07-05 19:40:11 +0200

    Export BasePostMessageStream

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ export * from './node-process/ProcessParentMessageStream';
 export * from './node-process/ProcessMessageStream';
 export * from './node-thread/ThreadParentMessageStream';
 export * from './node-thread/ThreadMessageStream';
-export { PostMessageEvent } from './BasePostMessageStream';
+export { PostMessageEvent, BasePostMessageStream } from './BasePostMessageStream';
 export { StreamData, StreamMessage } from './utils';
