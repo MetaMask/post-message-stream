f2e0a7f31399c0ea188146aaa8a4c6e196e266a0
Frederik Bolding <frederik.bolding@gmail.com>
2022-07-05 21:24:59 +0200

    Export `BasePostMessageStream` (#45)
    * Export BasePostMessageStream
    
    * Fix lint
    
    * Fix test
    
    * Export all

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ export * from './node-process/ProcessParentMessageStream';
 export * from './node-process/ProcessMessageStream';
 export * from './node-thread/ThreadParentMessageStream';
 export * from './node-thread/ThreadMessageStream';
-export { PostMessageEvent } from './BasePostMessageStream';
+export * from './BasePostMessageStream';
 export { StreamData, StreamMessage } from './utils';
