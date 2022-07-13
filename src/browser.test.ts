52fbc065356ef3e800c91674f2aeafacaca34bcf
Frederik Bolding <frederik.bolding@gmail.com>
2022-07-13 13:53:23 +0200

    BREAKING: Add browser entrypoint (#49)

---
+++ src/browser.test.ts
@@ -0,0 +1,22 @@
+import * as PostMessageStream from './browser';
+
+describe('post-message-stream', () => {
+  describe('browser exports', () => {
+    const expectedExports = [
+      'BasePostMessageStream',
+      'WindowPostMessageStream',
+      'WebWorkerPostMessageStream',
+      'WebWorkerParentPostMessageStream',
+    ];
+
+    it('package has expected exports', () => {
+      expect(Object.keys(PostMessageStream)).toHaveLength(
+        expectedExports.length,
+      );
+
+      for (const exportName of expectedExports) {
+        expect(exportName in PostMessageStream).toStrictEqual(true);
+      }
+    });
+  });
+});
---
+++ src/browser.ts
@@ -0,0 +1,6 @@
+// Exports a subset of functionality for browsers
+export * from './window/WindowPostMessageStream';
+export * from './WebWorker/WebWorkerPostMessageStream';
+export * from './WebWorker/WebWorkerParentPostMessageStream';
+export * from './BasePostMessageStream';
+export { StreamData, StreamMessage } from './utils';
