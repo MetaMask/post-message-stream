2a5e35218472fe97570f59e46c9383637c65aa23
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-12-16 13:01:38 +0100

    Export stream

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ export * from './node-process/ProcessParentMessageStream';
 export * from './node-process/ProcessMessageStream';
 export * from './node-thread/ThreadParentMessageStream';
 export * from './node-thread/ThreadMessageStream';
+export * from './runtime/RuntimePostMessageStream';
 export * from './BasePostMessageStream';
 export { StreamData, StreamMessage } from './utils';
