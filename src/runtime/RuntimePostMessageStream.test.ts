ab9e9a2e4e513e515a54eb9674b7d6233c7a544d
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-12-16 11:54:10 +0100

    Add Chrome runtime post message stream

---
+++ src/runtime/RuntimePostMessageStream.test.ts
@@ -0,0 +1,81 @@
+import { RuntimePostMessageStream } from './RuntimePostMessageStream';
+
+describe('RuntimePostMessageStream', () => {
+  beforeEach(() => {
+    const addListener = jest.fn();
+    const sendMessage = jest.fn().mockImplementation((message) => {
+      // Propagate message to all listeners.
+      addListener.mock.calls.forEach(([listener]) => {
+        listener(message);
+      });
+    });
+
+    Object.assign(global, {
+      chrome: {
+        runtime: {
+          sendMessage,
+          onMessage: {
+            addListener,
+            removeListener: jest.fn(),
+          },
+        },
+      },
+    });
+  });
+
+  it('throws if chrome.runtime.sendMessage is not a function', () => {
+    // @ts-expect-error - Invalid function type.
+    chrome.runtime.sendMessage = undefined;
+
+    expect(
+      () => new RuntimePostMessageStream({ name: 'foo', target: 'bar' }),
+    ).toThrow(
+      'chrome.runtime.sendMessage is not a function. This class should only be instantiated in a web extension.',
+    );
+  });
+
+  it('can communicate between streams and be destroyed', async () => {
+    // Initialize sender stream
+    const streamA = new RuntimePostMessageStream({
+      name: 'a',
+      target: 'b',
+    });
+
+    // Initialize receiver stream. Multiplies incoming values by 5 and
+    // returns them.
+    const streamB = new RuntimePostMessageStream({
+      name: 'b',
+      target: 'a',
+    });
+
+    streamB.on('data', (value) => streamB.write(value * 5));
+
+    // Get a deferred Promise for the result
+    const responsePromise = new Promise((resolve) => {
+      streamA.once('data', (num) => {
+        resolve(Number(num));
+      });
+    });
+
+    // Write to stream A, triggering a response from stream B
+    streamA.write(111);
+
+    expect(await responsePromise).toStrictEqual(555);
+
+    const throwingListener = (data: any) => {
+      throw new Error(`Unexpected data on stream: ${data}`);
+    };
+
+    streamA.once('data', throwingListener);
+    streamB.once('data', throwingListener);
+
+    // TODO.
+    // window.dispatchEvent(new Event('message'));
+
+    // Destroy streams and confirm that they were destroyed
+    streamA.destroy();
+    streamB.destroy();
+    expect(streamA.destroyed).toStrictEqual(true);
+    expect(streamB.destroyed).toStrictEqual(true);
+  });
+});
---
+++ src/runtime/RuntimePostMessageStream.ts
@@ -0,0 +1,77 @@
+import {
+  BasePostMessageStream,
+  PostMessageEvent,
+} from '../BasePostMessageStream';
+import { isValidStreamMessage } from '../utils';
+
+const noop = () => undefined;
+
+interface RuntimePostMessageStreamArgs {
+  name: string;
+  target: string;
+}
+
+/**
+ * A {@link chrome.runtime} stream.
+ */
+export class RuntimePostMessageStream extends BasePostMessageStream {
+  #name: string;
+
+  #target: string;
+
+  /**
+   * Creates a stream for communicating with other streams across the extension
+   * runtime.
+   *
+   * @param args - Options bag.
+   * @param args.name - The name of the stream. Used to differentiate between
+   * multiple streams sharing the same runtime.
+   * @param args.target - The name of the stream to exchange messages with.
+   */
+  constructor({ name, target }: RuntimePostMessageStreamArgs) {
+    super();
+
+    if (
+      typeof chrome === 'undefined' ||
+      typeof chrome?.runtime?.sendMessage !== 'function'
+    ) {
+      throw new Error(
+        'chrome.runtime.sendMessage is not a function. This class should only be instantiated in a web extension.',
+      );
+    }
+
+    this.#name = name;
+    this.#target = target;
+    this._onMessage = this._onMessage.bind(this);
+
+    chrome.runtime.onMessage.addListener(this._onMessage);
+
+    this._handshake();
+  }
+
+  protected _postMessage(data: unknown): void {
+    chrome.runtime.sendMessage(
+      {
+        target: this.#target,
+        data,
+      },
+      {},
+      // `runtime.sendMessage` assumes a response is sent directly from the
+      // target, but we don't need a response here. Not providing a callback
+      // would cause the function to return a promise, which we don't want.
+      noop,
+    );
+  }
+
+  private _onMessage(message: PostMessageEvent): void {
+    if (!isValidStreamMessage(message) || message.target !== this.#name) {
+      return;
+    }
+
+    this._onData(message.data);
+  }
+
+  _destroy(): void {
+    chrome.runtime.onMessage.removeListener(this._onMessage);
+  }
+}
