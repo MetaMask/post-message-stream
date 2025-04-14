3eda1ad9865a7aca7eb5382a6074328b9fd661f0
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-04-14 12:32:08 +0200

    Update build-test script

--- scripts/build-test.sh
+++ scripts/build-test.sh
@@ -7,6 +7,6 @@ set -o pipefail
 
 mkdir -p dist-test
 rm -rf dist-test/*
-browserify --standalone PostMessageStream ./dist/WebWorker/WebWorkerPostMessageStream.js > ./dist-test/WebWorkerPostMessageStream.js
-browserify --standalone PostMessageStream ./dist/node-process/ProcessMessageStream.js > ./dist-test/ProcessMessageStream.js
-browserify --im --node --standalone PostMessageStream ./dist/node-thread/ThreadMessageStream.js > ./dist-test/ThreadMessageStream.js
+browserify --standalone PostMessageStream ./dist/WebWorker/WebWorkerPostMessageStream.cjs > ./dist-test/WebWorkerPostMessageStream.js
+browserify --standalone PostMessageStream ./dist/node-process/ProcessMessageStream.cjs > ./dist-test/ProcessMessageStream.js
+browserify --im --node --standalone PostMessageStream ./dist/node-thread/ThreadMessageStream.cjs > ./dist-test/ThreadMessageStream.js
